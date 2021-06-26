16bd06b7c733c28e8c4d77ac5e2a7917382395bb
TibiNonEst <37983572+TibiNonEst@users.noreply.github.com>
2021-06-26 11:33:14 -0400

    Make worlds configurable, only query time zone once

--- src/main/java/me/tibinonest/plugins/currenttime/CurrentTime.java
+++ src/main/java/me/tibinonest/plugins/currenttime/CurrentTime.java
@@ -2,23 +2,48 @@ package me.tibinonest.plugins.currenttime;
 
 import org.bukkit.GameRule;
 import org.bukkit.World;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.time.ZoneId;
+import java.util.LinkedList;
 import java.util.List;
 
 public final class CurrentTime extends JavaPlugin {
 
     public static List<World> worlds;
     public static BukkitTask task;
+    public static Configuration config;
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
+        config = this.getConfig();
 
-        worlds = this.getServer().getWorlds().stream().filter(world -> world.getEnvironment().equals(World.Environment.NORMAL)).toList();
+        List<World> configWorlds = new LinkedList<>();
+        config.getStringList("worlds").forEach(world -> configWorlds.add(this.getServer().getWorld(world)));
+        if (configWorlds.contains(null)) {
+            this.getLogger().warning(config.getString("messages.world-not-found"));
+            return;
+        }
+        worlds = configWorlds.isEmpty() ? this.getServer().getWorlds().stream().filter(world -> world.getEnvironment().equals(World.Environment.NORMAL)).toList() : configWorlds;
         worlds.forEach(world -> world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false));
-        task = new GetTimeRunnable(this).runTaskTimer(this, 0, 20);
+
+        String configZone = config.getString("time-zone");
+        ZoneId zone;
+        try {
+            assert configZone != null;
+            zone = configZone.equals("") ? ZoneId.systemDefault() : ZoneId.of(configZone);
+        } catch (Exception e) {
+            this.getLogger().warning(config.getString("messages.zone-not-found"));
+            return;
+        }
+        task = new GetTimeRunnable(worlds, zone).runTaskTimer(this, 0, 20);
+
+        List<String> worldNames = new LinkedList<>();
+        worlds.forEach(world -> worldNames.add(world.getName()));
+        this.getLogger().info("Current time plugin started on world(s) " + String.join(", ", worldNames) + " on time zone " + zone);
     }
 
     @Override
@@ -27,7 +52,7 @@ public final class CurrentTime extends JavaPlugin {
     }
 
     public static void setTime(List<World> worlds, int time) {
-        worlds.forEach(world -> world.setTime(time));
+        worlds.forEach(world -> world.setTime(fixTime(time)));
     }
 
     public static int fixTime(int time) {
--- src/main/java/me/tibinonest/plugins/currenttime/GetTimeRunnable.java
+++ src/main/java/me/tibinonest/plugins/currenttime/GetTimeRunnable.java
@@ -1,31 +1,24 @@
 package me.tibinonest.plugins.currenttime;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.time.ZoneId;
+import java.util.List;
 
 public class GetTimeRunnable extends BukkitRunnable {
 
-    private final JavaPlugin plugin;
+    private final List<World> worlds;
+    private final ZoneId zone;
 
-    public GetTimeRunnable(JavaPlugin plugin) {
-        this.plugin = plugin;
+    public GetTimeRunnable(List<World> worlds, ZoneId zone) {
+        this.worlds = worlds;
+        this.zone = zone;
     }
 
     @Override
     public void run() {
-        String configZone = plugin.getConfig().getString("time-zone");
-        ZoneId zone;
-        try {
-            assert configZone != null;
-            zone = configZone.equals("") ? ZoneId.systemDefault() : ZoneId.of(configZone);
-        } catch (Exception e) {
-            plugin.getLogger().warning(plugin.getConfig().getString("messages.zone-not-found"));
-            this.cancel();
-            return;
-        }
         int time = (int) Math.floor(java.time.LocalTime.now(zone).toSecondOfDay() / 3.6);
-        CurrentTime.setTime(CurrentTime.worlds, time - 6000);
+        CurrentTime.setTime(worlds, time - 6000);
     }
 }
