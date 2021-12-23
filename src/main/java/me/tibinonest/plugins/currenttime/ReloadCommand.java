74227e3d54b58fc0a9c1f37c3d45bd2696564cfa
tibs <37983572+TibiNonEst@users.noreply.github.com>
2021-12-23 12:55:51 -0500

    Add @NotNull annotations to ReloadCommand
    Stops overriding annotations of CommandExecutor#onCommand

--- src/main/java/me/tibinonest/plugins/currenttime/ReloadCommand.java
+++ src/main/java/me/tibinonest/plugins/currenttime/ReloadCommand.java
@@ -3,6 +3,7 @@ package me.tibinonest.plugins.currenttime;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 public class ReloadCommand implements CommandExecutor {
     private final CurrentTime plugin;
@@ -12,7 +13,7 @@ public class ReloadCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         plugin.handleReload(sender);
         return true;
     }
